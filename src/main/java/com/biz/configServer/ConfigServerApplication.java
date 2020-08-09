2481410ac020a98d79e74dcdcb7133d523c45bc7
eomsh <oshnew@naver.com>
2020-08-09 18:22:12 +0900

    mysql 연동 작업소스 커밋(ing)

--- src/main/java/com/biz/configServer/ConfigServerApplication.java
+++ src/main/java/com/biz/configServer/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.biz.msa;
+package com.biz.configServer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
---
+++ src/test/java/com/biz/configServer/MsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
